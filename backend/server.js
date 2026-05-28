75c2ae989052af822803a4d2ee311a700cc49251
Saima Saraswat <saimasaraswat2305@gmail.com>
2026-05-28 13:28:40 +0530

    add build and prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,19 @@ import cors from 'cors';
 import "dotenv/config";
 import { connectDB } from './config/db.js';
 import userRouter from './routes/userRoute.js';
-import incomeRouter from './routes/incomeRoute.js'
+import incomeRouter from './routes/incomeRoute.js';
 import expenseRouter from './routes/expenseRoute.js';
 import dashboardRouter from './routes/dashboardRoute.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//middlewares
+// middlewares
 app.use(cors({
   origin: ["http://localhost:3000", "http://localhost:5173"],
   credentials: true
@@ -20,21 +23,21 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//db
+// db
 connectDB();
 
-
-
-//routes
+// routes
 app.use("/api/user", userRouter);
 app.use("/api/income", incomeRouter);
-app.use("/api/expense" , expenseRouter);
-app.use("/api/dashboard" , dashboardRouter)
-app.get("/", (req,res)=>{
-  res.send("Welcome to Expense Tracker API");
+app.use("/api/expense", expenseRouter);
+app.use("/api/dashboard", dashboardRouter);
+
+// serve frontend
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
